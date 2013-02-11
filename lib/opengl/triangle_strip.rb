73f8e0f3975b27f86605b32bb30dd4fd2f712086
Mark Mandel <mark.mandel@gmail.com>
2013-02-11 16:41:25 +1100

    refactor to use a VAO

--- lib/opengl/triangle_strip.rb
+++ lib/opengl/triangle_strip.rb
@@ -29,6 +29,7 @@ class OpenGL::TriangleStrip
 
 		@program_id = compile_program 'basic_vertex.glsl', 'basic_fragment.glsl'
 		init_vertex_buffers
+		init_vertex_array_objects
 
 		render_loop { display }
 
@@ -47,28 +48,21 @@ class OpenGL::TriangleStrip
 		#clear the buffer. Remember that Java static types come back as Ruby Constants.
 		GL11.gl_clear(GL11::GL_COLOR_BUFFER_BIT)
 		GL20.gl_use_program(@program_id)
+		GL30.gl_bind_vertex_array(@vao_id)
 
-		GL15.gl_bind_buffer(GL15::GL_ARRAY_BUFFER, @buffer_id)
-
-		GL20.gl_enable_vertex_attrib_array(0)
-		GL20.gl_vertex_attrib_pointer(0, 4, GL11::GL_FLOAT, false, 0, 0)
+		GL15.gl_bind_buffer(GL15::GL_ARRAY_BUFFER, @vertex_buffer_id)
 
 		GL11.gl_draw_arrays(GL11::GL_TRIANGLE_STRIP, 0, 4)
 
 		GL15.gl_bind_buffer(GL15::GL_ARRAY_BUFFER, 0)
-		GL20.gl_disable_vertex_attrib_array(0)
 		GL20.gl_use_program(0)
-
+		GL30.gl_bind_vertex_array(0)
 
 	end
 
-
 	def init_vertex_buffers
-		@vao_id = GL30.gl_gen_vertex_arrays
-		GL30.gl_bind_vertex_array(@vao_id)
-
-		@buffer_id = GL15.gl_gen_buffers
-		GL15.gl_bind_buffer(GL15::GL_ARRAY_BUFFER, @buffer_id)
+		@vertex_buffer_id = GL15.gl_gen_buffers
+		GL15.gl_bind_buffer(GL15::GL_ARRAY_BUFFER, @vertex_buffer_id)
 
 		float_buffer = BufferUtils.create_float_buffer(@vertex_data.size)
 		float_buffer.put(@vertex_data.to_java(:float))
@@ -80,6 +74,18 @@ class OpenGL::TriangleStrip
 
 		# cleanup
 		GL15.gl_bind_buffer(GL15::GL_ARRAY_BUFFER, 0)
+	end
+
+	# initialise the vertex array objects
+	def init_vertex_array_objects
+		#first object
+		@vao_id = GL30.gl_gen_vertex_arrays
+		GL30.gl_bind_vertex_array(@vao_id)
+
+		GL15.gl_bind_buffer(GL15::GL_ARRAY_BUFFER, @vertex_buffer_id)
+		GL20.gl_enable_vertex_attrib_array(0)
+		GL20.gl_vertex_attrib_pointer(0, 4, GL11::GL_FLOAT, false, 0, 0)
+
 		GL30.gl_bind_vertex_array(0)
 	end
 
